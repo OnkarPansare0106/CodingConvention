39b139dfd8afb8fec9d9510cb134a84816e79751
Onkar Pansare <Q6848@Corp.qualitykiosk.com>
2021-02-26 20:25:37 +0530

    File added

---
+++ Employee1.java
@@ -0,0 +1,32 @@
+package CodingConvention;
+
+public class Employee1 {
+    int id;
+    double salary=0.0;
+
+
+    public Employee1(int Id) {
+        this.id = id;
+    }
+
+    public Employee1() {
+    }
+
+    double calTax(){
+        if(this.salary > 500000.00 && this.salary < 1000000)
+            return this.salary*0.1;
+
+        else if(this.salary >1000000.00 && this.salary < 1500000) {
+            return this.salary * 0.2;
+        }
+        else
+            return this.salary*0.3;
+    }
+
+    public static void main(String[] args) {
+        for(int index=0;index<10;index++){
+
+        }
+    }
+
+}
